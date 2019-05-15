4f41e1604443d01d6e38e3ffd36d3cd815e2c7a4
Jablan <joshuaablan@gmail.com>
2019-05-14 22:05:04 -0700

    add team wins and times

--- src/component/TourneyContainer/Tournaments.js
+++ src/component/TourneyContainer/Tournaments.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import moment from "moment";
 
-const boxDiv = {
-    
-}
+
 
 const ListTournaments = (props) => 
     <li>
         {props.tourney.league.name} - {props.tourney.name.toUpperCase()}  <br/>
         <img src={props.tourney.league.image_url} width="150" height="150"alt=""/>
     </li>
+
+const ListTeams = (props) =>
+        props.tourney.teams.map((team,i)=>
+            <li key={i}>
+                {team.name} <br/>
+                <img src={team.image_url} width="150" height="150"alt=""/>
+            </li>
+        )
     
 
 const Tournaments = (props) => {
@@ -27,37 +34,39 @@ const Tournaments = (props) => {
         upcomingList = dataUpcoming.map((tourney, i) => 
             <ListTournaments tourney={tourney} key={i} />
         )
+        // upcomingTeamsList = dataUpcoming.map((tourney, i) => 
+        //     <ListTournaments tourney={tourney} key={i} />
+        // )
+
         pastList = dataPast.map((tourney, i) => 
             <ListTournaments tourney={tourney} key={i} />
         )
-        teamsList = dataRunning.map((tourney,i)=> 
-            tourney.teams.map((team, i)=>
-            <li key={i}>
-                {team.name} <br/>
-                <img src={team.image_url} width="150" height="150"alt=""/>
-            </li>
-            )
+        teamsList = dataRunning.map((tourney, i)=>
+            <ListTeams tourney={tourney} key={i}/>
         )
         pastMatchesList = dataPastMatches.map((match,i)=>{
+            let dateBegin = new Date(match.begin_at).toDateString()
+            let timeBegin = moment(new Date(match.begin_at).toTimeString().split("G")[0],"hh,mm,ss").format("h:mm A")
             return(
-                <div key={i}>
-                    <h4>{match.name}</h4>
-                    {
-                        match.opponents.map((opponent, i)=>
-                        <li key={i}>
-                            {opponent.opponent.name} <br/>
-                            <img src={opponent.opponent.image_url} width="150" height="150"alt=""/>
-                        </li>
-                        )
-                    }
-                </div>
-                
-
-            )
-
-        }
+                    <div key={i}>
+                        <div>
+                            <h4>{match.name}</h4>
+                            <h6>{dateBegin}</h6>
+                            <h6>{timeBegin}</h6>
+                        </div>
+                        {
+                            match.opponents.map((opponent, i)=>
+                            <li key={i}>
+                                {opponent.opponent.name} <br/>
+                                <h6>WINS:{match.results[i].score}</h6>
+                                <img src={opponent.opponent.image_url} width="50" height="50"alt=""/>
+                            </li>
+                            )
+                        }
+                    </div>
+                )
+            }
         )
-
     }
     return (
         <div>
@@ -71,7 +80,7 @@ const Tournaments = (props) => {
                         <ul>
                             {runningList}
                         </ul>
-                        <h3>Upcoming tournaments</h3>
+                        <h3>Upcoming Matches</h3>
                         <ul>
                             {upcomingList}
                         </ul>
@@ -96,7 +105,6 @@ const Tournaments = (props) => {
             )
             : <h1>Loading...</h1>
             }
-            
         </div>
     )
    
